54c64709465e0d1dc96198b17559eeb427dd2d14
Jaime-Ginesky <jaimelee79@gmail.com>
2024-01-29 12:28:21 -0800

    integrated api for city and temperature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+ function searchCity(city) {
+    // make api call and update the interface
+    let apiKey = `f3b5c0e756b3t43ba00423f6a67boe48`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event) {
    event.preventDefault();
    let searchInput  = document.querySelector("#search-form-input");
    let cityElement = document.querySelector("#city");
    cityElement.innerHTML = searchInput.value;
+   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
