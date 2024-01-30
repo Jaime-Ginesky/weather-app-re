ed9e550d8f37a0dc62ca9b4b6951af5e29a4fe36
Jaime-Ginesky <jaimelee79@gmail.com>
2024-01-29 16:56:34 -0800

    injected time and date

--- src/index.js
+++ src/index.js
@@ -3,8 +3,44 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    
+    console.log(response.data);
+    
+    
+    timeElement.innerHTML = formatDate(date);
+    windElement.innerHTML = `${response.data.wind.speed}mph`;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    descriptionElement.innerHTML = response.data.condition.description;
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
+
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`
+
 }
 
  function searchCity(city) {
@@ -18,11 +54,13 @@ function refreshWeather(response) {
 function handleSearchSubmit(event) {
    event.preventDefault();
    let searchInput  = document.querySelector("#search-form-input");
-   let cityElement = document.querySelector("#city");
-   cityElement.innerHTML = searchInput.value;
+
    searchCity(searchInput.value);
+
 }
 
-let searchFormElement = document.querySelector("#search-form");
+
+let searchFormElement = document.querySelector("#search-form-submit");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+searchCity("Los Angeles");
